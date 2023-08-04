09b15125e97e0c475c1a2722ff55cbca82f26cc5
asperjay <jasperelsley@gmail.com>
2023-08-03 20:48:39 -0700

    create swift files

---
+++ alarmclock/alarmclock/alarmclockApp.swift
@@ -0,0 +1,17 @@
+//
+//  alarmclockApp.swift
+//  alarmclock
+//
+//  Created by Jasper Elsley on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct alarmclockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
